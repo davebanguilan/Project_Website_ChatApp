393821c166fb2bec12fc763c2b5c8dd8ad6e5f97
davebanguilan <desbanguilan@gmail.com>
2021-01-30 22:44:01 +0800

    Added MyMessage, TheirMessage, MessageForm, and LoginForm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {ChatEngine} from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed'
+
 import './App.css'
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return(
         <ChatEngine 
             height="100vh"
-            projectID={process.env.REACT_APP_PROJECTID}
-            userName={process.env.REACT_APP_USERNAME}
-            userSecret={process.env.REACT_APP_USERSECRET}
+            projectID="318278f9-5422-4b50-bb8b-7b702464cba2"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
 }
